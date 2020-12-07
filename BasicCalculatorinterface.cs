f3325df711e08e30f52e3905bf361f57187d216c
hamim932 <72722819+hamim932@users.noreply.github.com>
2020-12-07 07:47:52 +0600

    Add files via upload

---
+++ BasicCalculatorinterface.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final-Lab2
+{
+    interface BasicCalculatorInterface
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+
+    }
+}
