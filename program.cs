e3e2d9fc15a6680f77d631ba20bd3df7b8567d8a
hamim932 <72722819+hamim932@users.noreply.github.com>
2020-12-07 07:52:36 +0600

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Final_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(" Sum = " + c.sum(15, 10));
+            Console.WriteLine(" Sub = " + c.sub(15, 10));
+            Console.WriteLine(" Multiplication = " + c.multiplication(15, 10));
+            Console.WriteLine(" Division = " + c.division(15, 10));
+            Console.WriteLine(" Power = " + c.XtoY(15, 10));
+            Console.WriteLine(" Factorial = " + c.factorial(10));
+        }
+    }
+}
