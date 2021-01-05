efaf72a89ac0929e8dac8383ced07b7ecae4d86f
hamim932 <72722819+hamim932@users.noreply.github.com>
2021-01-05 09:28:06 +0600

    Add files via upload

---
+++ IScientificalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_second_lab
+{
+    interface IScientificCalculatorInterface
+    {
+        int XtoY(int x, int y);
+        double SquareRoot(double x);
+
+
+    }
+}
